37ad193164fecf30d754b9384eed68ffcf34161d
DJLougen <dlougen2@uoregon.edu>
2023-01-22 06:42:26 -0800

    message

---
+++ CArtpole.py
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get install -y xvfb ffmpeg freeglut3-dev
+pip install 'imageio==2.4.0'
+pip install pyvirtualdisplay
+pip install tf-agents[reverb]
+pip install pyglet
---
+++ TF_RL_tutorial.py
@@ -0,0 +1,14 @@
+#policy: dictates action taken by the agenty in a given environment 
+  #s is the current observation of the environment
+  #s' is the state at the next step
+  #p is policy 
+  #r is reward 
+  #(Q(s,a)) is the action-value function 
+    #expected total reward starting from current state, which outputs the Q value. MAXIMIZE THIS.
+  
+  
+
+#Cartpole:
+  #y discount factor: discounts future rewards relatibe to immediate ones.
+    #Relates rewards to the time domain
+  
