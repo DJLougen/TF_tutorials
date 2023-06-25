fe5e105fa328cbe79f6dbbe7de0a9929a4579fba
djlougen <dlougen2@uoregon.edu>
2023-06-25 09:00:52 -0700

    updated to account for categorical variables

--- KAggle data/ds_salary_seq.py
+++ KAggle data/ds_salary_seq.py
@@ -1,10 +1,13 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
+import numpy as np
 
+# Make numpy values easier to read.
+np.set_printoptions(precision=3, suppress=True)
+
+import tensorflow as tf
+from tensorflow.keras import layers
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
 # Load in data
 data = pd.read_csv("KAggle data/ds_salaries.csv")
 
@@ -22,23 +25,32 @@ data.head()
 # company_size: The median number of people that worked for the company during the year
 
 
+  inputs[name] = tf.keras.Input(shape=(1,), name=name, dtype=dtype)
 # Split into features and labels
-X = data.iloc[:, :-1]  # Features (with column titles)
-y = data.iloc[:, -1]  # Labels
+data_feature = data.copy()
+data_label = data.pop('salary_currency') # predicting salary of DS
 
-# Test/Train
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=420
-)  # Random state = seed
+#Pre-process
+inputs = {} 
+    #For loop to match data to type
+for name, column in data.items():
+  dtype = column.dtype
+  if dtype == object:
+    dtype = tf.string
+  else:
+    dtype = tf.float32
 
+#Pack features into array
+data_feature = np.array(data_feature)
 
-# Define the model
-model = keras.models.Sequential()
-model.add(keras.layers.Dense(64, input_shape=(X_train.shape[1],), activation="relu"))
-# 64 neurons
-# Specifies the shape of the input into the first layer
-# relu = Rectified Linear Unit
-model.add(keras.layers.Dense(64, activation="relu"))  # Takes input from prior layer
-model.add(
-    keras.layers.Dense(1, activation="sigmoid")
-)  # Classification requires 1 neuron output because binary, sigmoid common for binary problems
+#Regression model 
+model = tf.keras.Sequential([
+    layers.Dense(64),
+    layers.Dense(1)
+])
+
+#Compile data 
+model.compile(loss = tf.keras.losses.MeanSquaredError(),
+                optimizer = tf.keras.optimizers.Adam())
+#Train model 
+model.fit(data_feature, data_label, epochs= 100)
